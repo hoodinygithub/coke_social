f692a44d17e1e0aaa0ff1eefec4910c5d905e616
Steven Nedlin <snedlin@hoodiny.com>
2010-05-06 15:58:47 -0400

    Added top_djs and top_playlists models and associations.

---
+++ db/migrate/20100506193943_add_top_playlists_and_djs.rb
@@ -0,0 +1,27 @@
+class AddTopPlaylistsAndDjs < ActiveRecord::Migration
+  def self.up
+    create_table :top_djs do |t|
+      t.references :dj
+      t.references :site
+      t.integer :total_requests
+      t.timestamps
+    end
+    add_index :top_djs, [:site_id, :request_count]
+    add_index :top_djs, :dj_id
+
+    create_table :top_playlists do |t|
+      t.references :playlist
+      t.references :site
+      t.integer :total_requests
+      t.timestamps
+    end
+    add_index :top_playlists, [:site_id, :request_count]
+    add_index :top_playlists, :playlist_id
+
+  end
+
+  def self.down
+    drop_table :top_djs
+    drop_table :top_playlists
+  end
+end
