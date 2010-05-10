1c16a7863c99da569627d8782993d5abded591fc
Jefferson Girao <contato@jefferson.eti.br>
2010-05-10 19:14:36 -0300

    Followers/Following pagination fix

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -12,10 +12,10 @@ class FollowersController < ApplicationController
         sort_types  = { :latest => 'followings.approved_at DESC', :alphabetical => 'accounts.name'  }
         if profile_artist?
           @pending    = profile_artist.follow_requests
-          @collection = profile_artist.followers.paginate :page => params[:page], :per_page => 12, :order => sort_types[@sort_type]
+          @collection = profile_artist.followers.paginate :page => params[:page], :per_page => 15, :order => sort_types[@sort_type]
         else
           @pending    = profile_user.follow_requests
-          @collection = profile_user.followers.paginate :page => params[:page], :per_page => 12, :order => sort_types[@sort_type]
+          @collection = profile_user.followers.paginate :page => params[:page], :per_page => 15, :order => sort_types[@sort_type]
         end
     rescue NoMethodError
       redirect_to new_session_path
