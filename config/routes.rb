4552e77b24ff9935f94cca71719a3242160d59e9
Elvio Junior <elvio@bd232a2c.virtua.com.br>
2010-05-11 15:51:04 -0300

    Added missing links to reviews and subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -162,7 +162,9 @@ ActionController::Routing::Routes.draw do |map|
     profile.resources :following, :controller => 'followees'
     profile.resources :followers
     profile.resources :activities, :only => :index
+
     profile.resources :subscriptions
+    profile.resources :reviews
 
     profile.resources :charts, :only => :index
     #profile.charts 'charts', :controller => 'charts', :action => 'index'
