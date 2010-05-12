889759a480a713d2ce62407da3d3b0e4de55ade8
Jefferson Girao <contato@jefferson.eti.br>
2010-05-12 09:57:19 -0300

    Marking default sorting at playlist controller

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -6,7 +6,6 @@ class PlaylistsController < ApplicationController
 
   def index
     @sort_type = params.fetch(:sort_by, nil).to_sym rescue :latest
-
     begin
       sort_types  = { :latest => 'playlists.created_at DESC', 
                       :alphabetical => 'playlists.name',
