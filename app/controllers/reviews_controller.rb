0bb46fa40b904ac09301ac3669568413cc81e443
Jefferson Girao <contato@jefferson.eti.br>
2010-05-14 12:52:48 -0300

    Fixing reviews default sorting

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   def index
+    @sort_type = params.fetch(:sort_by, nil).to_sym rescue :latest
     @section = "reviews_page"
     @collection = []
   end
