455210119f1fdb9ba65149961badd42babcdbcdc
Steven Nedlin <snedlin@hoodiny.com>
2010-05-20 16:11:23 -0400

    Data encryption. Update to registration to parse born_on date.

--- db/migrate/20100520003517_add_encrypted_fields_to_accounts.rb
+++ db/migrate/20100520003517_add_encrypted_fields_to_accounts.rb
@@ -1,9 +1,9 @@
 class AddEncryptedFieldsToAccounts < ActiveRecord::Migration
   def self.up
-    ActiveRecord::Base.connection.execute "ALTER TABLE accounts ADD encrypted_born_on_string varchar(255), ADD encrypted_name varchar(255), ADD encrypted_gender varchar(255), ADD encrypted_email varchar(255), ADD KEY `index_accounts_on_encrypted_email` (encrypted_email)"
+    ActiveRecord::Base.execute "ALTER TABLE accounts ADD encrypted_born_on_string varchar(255), ADD encrypted_name varchar(255), ADD encrypted_gender varchar(255), ADD encrypted_email varchar(255), ADD KEY `index_accounts_on_encrypted_email` (encrypted_email)"
   end
 
   def self.down
-    ActiveRecord::Base.connection.execute "ALTER TABLE accounts DROP KEY `index_accounts_on_encrypted_email`, DROP COLUMN encrypted_born_on_string, DROP COLUMN encrypted_name, DROP COLUMN encrypted_gender, DROP COLUMN encrypted_email"
+    ActiveRecord::Base.execute "ALTER TABLE accounts DROP KEY `index_accounts_on_encrypted_email`, DROP COLUMN encrypted_born_on_string, DROP COLUMN encrypted_name, DROP COLUMN encrypted_gender, DROP COLUMN encrypted_email"
   end
 end
