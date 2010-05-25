0ab4f0b0c18fe3ca692696cb745ec18c3e6dc978
Steven Nedlin <snedlin@hoodiny.com>
2010-05-24 20:30:45 -0400

    Added back born_on encryption key.

--- app/models/account/encryption.rb
+++ app/models/account/encryption.rb
@@ -3,7 +3,7 @@ module Account::Encryption
     base.class_eval do
       attr_encrypted :email, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4'
       attr_encrypted :name, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4'
-      attr_encrypted :born_on_string, '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4'
+      attr_encrypted :born_on_string, :key =>'1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4'
       attr_encrypted :gender, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4'
     end
   end
