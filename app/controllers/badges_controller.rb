ed1a4b9283ab05984f549206d3a0216a4fbe5e10
Steven Nedlin <snedlin@hoodiny.com>
2010-05-26 00:03:01 -0400

    Replaced the rescues from badge controller and fixed badge detail.

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -3,22 +3,22 @@ class BadgesController < ApplicationController
     @dashboard_menu = :badges
     @sort_type = params.fetch(:sort_by, nil).to_sym rescue :latest
 
-    #begin
+    begin
         sort_types = { :latest => 'badge_awards.created_at DESC', :alphabetical => "badge_awards.name_#{current_site.default_locale.to_s.downcase}"  }
         @collection = profile_user.badge_awards.paginate :page => params[:page], :per_page => 10, :order => sort_types[@sort_type]
-    #rescue NoMethodError
-    #  redirect_to new_session_path
-    #end
+    rescue NoMethodError
+      redirect_to new_session_path
+    end
   end
 
   def show
     @dashboard_menu = :badges
-    #begin
+    begin
       @badge_award = profile_user.badge_awards.find(params[:id])
       @friends_with_badge = profile_user.followees.with_badge(@badge_award)
       @others_with_badge = @badge_award.badge.winners.other_than_user_and_followees(profile_user)
-    #rescue
-     # redirect_to new_session_path
-    #end
+    rescue
+      redirect_to new_session_path
+    end
   end
 end
--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,7 +2,7 @@ class Badge < ActiveRecord::Base
   has_many :badge_awards
   has_many :winners, :through => :badge_awards, :class_name => 'User' do
     def other_than_user_and_followees(user)
-      all(:conditions => "accounts.id NOT IN (#{user.id}#{user.followee_cache.empty? ? "" : ",#{user.followee_cache.join(',')}"})")
+      all(:conditions => ["accounts.id NOT IN (?)", [user.id].concat(user.read_attribute(:followee_cache)])
     end
   end
 
