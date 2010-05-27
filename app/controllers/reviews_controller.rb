690a722b1ddd4c51fb81f933c06df6fe0446099f
Jefferson Girao <contato@jefferson.eti.br>
2010-05-27 18:44:08 -0300

    Fixing records loaded for private reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -96,7 +96,7 @@ protected
   def load_records
     conditions = {}
     conditions.merge!({ :commentable_type => 'Playlist', :commentable_id => params[:playlist_id] }) if params[:playlist_id]
-    conditions.merge!({ :user_id => profile_account.id }) if params[:slug]
+    conditions.merge!({ :user_id => profile_account.id }) if profile_account
     @playlist  = Playlist.find(params[:playlist_id]) if params[:playlist_id]
     @records   = Comment.all(:conditions => conditions, :order => @sort_data )
   end
