1e6d95656b92fed5c85927cc60d75542203b73f6
Jefferson Girao <contato@jefferson.eti.br>
2010-06-09 20:32:25 -0300

    Reviews records loading fix

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -108,7 +108,7 @@ protected
   def load_records
     conditions = {}
     conditions.merge!({ :commentable_type => 'Playlist', :commentable_id => params[:playlist_id] }) if params[:playlist_id]
-    conditions.merge!({ :user_id => profile_account.id }) if profile_account and !request.xhr?
+    conditions.merge!({ :user_id => profile_account.id }) unless request.request_uri.match(/playlists/)
     @playlist  = Playlist.find(params[:playlist_id]) if params[:playlist_id]
     @records   = Comment.all(:conditions => conditions, :order => @sort_data )
   end
