2398ac0f158ae2869ef33e4bd0a810ada43d0a7d
Steven Nedlin <snedlin@hoodiny.com>
2010-06-09 11:46:50 -0400

    Fix to array concat for nil followee cache.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,7 +2,7 @@ class Badge < ActiveRecord::Base
   has_many :badge_awards
   has_many :winners, :through => :badge_awards, :class_name => 'User' do
     def other_than_user_and_followees(user, limit=40)
-      followee_cache = [user.id].concat(user.read_attribute(:followee_cache))
+      followee_cache = [user.id].concat(user.read_attribute(:followee_cache) || [])
       all(:conditions => ["accounts.id NOT IN (?)", followee_cache], :order => 'badge_awards.created_at DESC', :limit => limit)
     end
   end
