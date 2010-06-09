c197b9c1c360aa46a7896452bc3110091783b09c
Steven Nedlin <snedlin@hoodiny.com>
2010-06-09 11:27:44 -0400

    Removed redirect to login in badges_controller.

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -3,23 +3,19 @@ class BadgesController < ApplicationController
     @dashboard_menu = :badges
     @sort_type = params.fetch(:sort_by, nil).to_sym rescue :latest
 
-    begin
-        sort_types = { :latest => 'badge_awards.created_at DESC', :alphabetical => "badge_awards.name_#{current_site.default_locale.to_s.downcase}"  }
-        @collection = profile_user.badge_awards.paginate :page => params[:page], :per_page => 10, :order => sort_types[@sort_type]
-    rescue NoMethodError
-      redirect_to new_session_path
+    sort_types = { :latest => 'badge_awards.created_at DESC', :alphabetical => "badge_awards.name_#{current_site.default_locale.to_s.downcase}"  }
+    @collection = profile_user.badge_awards.paginate :page => params[:page], :per_page => 10, :order => sort_types[@sort_type]
+
+    if request.xhr?
+      render :partial => 'list'
     end
   end
 
   def show
     @dashboard_menu = :badges
-    begin
-      @badge_award = profile_user.badge_awards.find(params[:id])
-      @friends_with_badge = profile_user.followees.with_badge(@badge_award)
-      @others_with_badge = @badge_award.badge.winners.other_than_user_and_followees(profile_user)
-    rescue
-      redirect_to new_session_path
-    end
+    @badge_award = profile_user.badge_awards.find(params[:id])
+    @friends_with_badge = profile_user.followees.with_badge(@badge_award)
+    @others_with_badge = @badge_award.badge.winners.other_than_user_and_followees(profile_user)
   end
   
   def set_notified
