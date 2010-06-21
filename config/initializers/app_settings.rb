74ec7abbb6abf86afc123e2187078ac0820e1f2a
Jefferson Girao <contato@jefferson.eti.br>
2010-06-21 16:18:28 -0300

    Site - Set up contact us inbox - Story ID #3146587

--- config/initializers/app_settings.rb
+++ config/initializers/app_settings.rb
@@ -1,3 +1,5 @@
 ENV['BR_FEEDBACK_EMAIL'] = "br.msn.cyloop.com@hoodiny.com"
 ENV['MX_FEEDBACK_EMAIL'] = "mx.msn.cyloop.com@hoodiny.com"
+ENV['COKE_BR_FEEDBACK_EMAIL'] = "coke.br.cyloop.com@hoodiny.com"
+ENV['COKE_LATAM_FEEDBACK_EMAIL'] = "coke.latam.cyloop.com@hoodiny.com"
 ENV['ASSETS_URL'] = 'http://assets.cyloop.com'
