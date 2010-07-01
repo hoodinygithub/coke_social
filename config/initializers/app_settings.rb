d6e900cb94ecbdd953cd02810ee6456e2c5bb59d
Jefferson Girao <contato@jefferson.eti.br>
2010-07-01 20:24:24 -0300

    Email - Cancel Account Confirmation - Story ID #3146549

--- config/initializers/app_settings.rb
+++ config/initializers/app_settings.rb
@@ -1,7 +1,7 @@
 ENV['BR_FEEDBACK_EMAIL'] = "br.msn.cyloop.com@hoodiny.com"
 ENV['MX_FEEDBACK_EMAIL'] = "mx.msn.cyloop.com@hoodiny.com"
-ENV['COKE_BR_FEEDBACK_EMAIL'] = "coke.br.cyloop.com@hoodiny.com"
-ENV['COKE_LATAM_FEEDBACK_EMAIL'] = "coke.latam.cyloop.com@hoodiny.com"
+ENV['COKEBR_FEEDBACK_EMAIL'] = "coke.br.cyloop.com@hoodiny.com"
+ENV['COKELATAM_FEEDBACK_EMAIL'] = "coke.latam.cyloop.com@hoodiny.com"
 ENV['ASSETS_URL'] = 'http://assets.cyloop.com'
 
 # ENV['RECAPTCHA_PUBLIC_KEY']  = '6LcqBbsSAAAAAJhIN9bZo5Suvk7FRdIbAeHPcWMI'
