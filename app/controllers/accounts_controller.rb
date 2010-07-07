59b4fe663a09eb49e4f91b9434d8f9eea3af4bfc
Jefferson Girao <contato@jefferson.eti.br>
2010-07-07 11:19:19 -0300

    Latest reviews module sort order fix

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -12,7 +12,7 @@ class AccountsController < ApplicationController
     @dashboard_menu = :home
     @mixes_recommended = (1..6).to_a
     @comments = (1..3).to_a
-    @reviews = profile_account.comments.valid({:limit => 5})
+    @reviews = profile_account.comments.valid({:limit => 5, :order => "comments.updated_at DESC"})
     
     @followers = profile_account.followers.all(:limit => 4)
 
