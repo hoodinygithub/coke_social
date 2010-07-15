6dc0d658e90606f6287d9c6e2952ef3e990f1ceb
Steven Nedlin <snedlin@hoodiny.com>
2010-07-15 18:11:14 -0400

    Adding migrations from cyloop_social

---
+++ db/migrate/20100715005456_add_optimizations_to_playlist_items.rb
@@ -0,0 +1,17 @@
+class AddOptimizationsToPlaylistItems < ActiveRecord::Migration
+  def self.up
+    ActiveRecord::Base.connection.execute <<-EOF
+    ALTER TABLE playlist_items
+    ADD artist_id int(11),
+    ADD KEY `index_playlists_items_on_updated_at_for_latest_sort_for_artists` (`artist_id`,`playlist_id`,`updated_at`)
+    EOF
+  end
+
+  def self.down
+    ActiveRecord::Base.connection.execute <<-EOF
+    ALTER TABLE playlist_items
+    DROP COLUMN artist_id
+    DROP KEY `index_playlists_items_on_updated_at_for_latest_sort_for_artists`
+    EOF
+  end
+end
