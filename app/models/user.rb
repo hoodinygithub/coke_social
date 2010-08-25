9248d7adfbe3a4dee89f9fe0ca5c350d305c3a04
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-08-25 12:54:48 -0300

    Only grab tags from current site

--- app/models/user.rb
+++ app/models/user.rb
@@ -176,11 +176,11 @@ class User < Account
     id <=> b.id
   end
 
-  def tag_counts_from_playlists(limit=60)
+  def tag_counts_from_playlists(current_site, limit=60)
     options = { :select => "DISTINCT tags.*",
-                :joins => "INNER JOIN #{Tagging.table_name} ON #{Tag.table_name}.id = #{Tagging.table_name}.tag_id INNER JOIN #{Playlist.table_name} ON #{Tagging.table_name}.taggable_id = #{Playlist.table_name}.id AND #{Tagging.table_name}.taggable_type = 'Playlist'",
+                :joins => "INNER JOIN #{Tagging.table_name} ON #{Tag.table_name}.id = #{Tagging.table_name}.tag_id INNER JOIN #{Playlist.table_name} ON #{Tagging.table_name}.taggable_id = #{Playlist.table_name}.id AND #{Tagging.table_name}.taggable_type = 'Playlist' INNER JOIN valid_tags ON valid_tags.tag_id = #{Tag.table_name}.id",
                 :order => "taggings.created_at DESC",
-                :conditions => "playlists.owner_id = #{self.id} AND playlists.deleted_at IS NULL",
+                :conditions => "playlists.owner_id = #{self.id} AND playlists.deleted_at IS NULL AND valid_tags.site_id = #{current_site.id}",
                 :limit => limit }
 
     Tag.all(options)
