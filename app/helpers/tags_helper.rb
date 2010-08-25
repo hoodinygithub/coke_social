b86aa67d6b03064e364fdd86efa49d2d069d5537
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-08-25 17:38:58 -0300

    Site locked tag fix

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -2,8 +2,6 @@ module TagsHelper
   def tag_cloud(tags, classes)
     return if tags.empty?
     
-    tags.delete_if{|tag| !current_site.tag_ids.include?(tag.id) }
-    
     max_count = tags.sort_by(&:taggings_count).last.taggings_count.to_f
     
     tags.each_with_index do |tag, index|
