112632c8371b36f8bad282b2ae9fbeac7289c20e
suezieq71 <christensen.sm@gmail.com>
2010-08-26 13:37:02 -0700

    Added another beta tester to white list.

--- app/models/account/authentication.rb
+++ app/models/account/authentication.rb
@@ -173,7 +173,7 @@ module Account::Authentication
     fferrazza@sapient.com mconigliaro@sapient.com sbamber@sapient.com ahollander@sapient.com jschneider@sapient.com mylena@gringo.nu 
     andre@gringo.nu ana.hernandes@jwt.com tullio.nicastro@jwt.com lucianam@ciandt.com claudia.caballero@starcom.com.mx gustavo.ortega@starcom.com.mx 
     hsartorelli@cubika.com iandino@proguidemc.com tonda@wannaflock.com barquin@wannaflock.com prieto@wannaflock.com molina@wannaflock.com leonardo@agenciaroja.com cova@agenciaroja.com leosecundo@hotmail.com seba@santo.net maxi@santo.net gaston.bigio@ogilvy.com carlos@madrebuenosaires.com gustavotaretto@gmail.com gabriel.vazquez@jwt.com 
-julian.smith@jwt.com anselmo.ramos@ogilvy.com jose.montalvo@ogilvy.com nescobedo@wallaby-group.com gvaldes@wallaby-group.com dom@fruktmusic.com jack@fruktmusic.com)
+julian.smith@jwt.com anselmo.ramos@ogilvy.com jose.montalvo@ogilvy.com nescobedo@wallaby-group.com gvaldes@wallaby-group.com dom@fruktmusic.com jack@fruktmusic.com floza@augeo.com.ar)
     if !valid_scan_emails.include?(email)
       valid_domains = ['ko.com', 'hoodiny.com', 'cyloop.com', 'clarusdigital.com', 'la.ko.com', 'mena.ko.com', 'na.ko.com', 'eur.ko.com', 'fruktmusic.com', 'sapient.com']
       errors.add(:email, I18n.t('share.errors.message.email_is_not_authorized') ) unless valid_domains.include?(email.split("@")[1])
