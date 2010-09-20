1c81e6c9d9fab826f6c58d4167cea528a0fccb51
Ed Toro <etoro@hoodiny.com>
2010-09-20 19:19:30 -0400

    Fix Ruby warnings on un-escaped '-' in regex.

--- app/models/account/website.rb
+++ app/models/account/website.rb
@@ -12,7 +12,7 @@ module Account::Website
 
   def websites=(ary)
     ary.each { |v| puts v }
-    ary.reject! { |v| !(/[A-Za-z0-9-_]+\.[A-Za-z0-9-_%&\?\/.=]+$/).match(v) }
+    ary.reject! { |v| !(/[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_%&\?\/.=]+$/).match(v) }
     ary.each { |v| v.gsub!('http://', '') }
     write_attribute(:websites, Array(ary).select{|v| !v.blank? }[0..4]) # write up to 5 non-blank items
   end
