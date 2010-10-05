6bc7ddb1d1dd7c2cd2adde5cffa3c4b639d9acdb
David Salazar <dsalazar@hoodiny.com>
2010-10-05 13:34:39 -0400

    Removed http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
 
   sanitize_params
 
-  before_filter :do_basic_http_authentication
+  # before_filter :do_basic_http_authentication
   before_filter :confirm_registration_code
   #before_filter :login_required  
 
