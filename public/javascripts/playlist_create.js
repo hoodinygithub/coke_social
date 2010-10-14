a32c9fc1741f4a76485ecbe3ffb4bbe698a6ec6b
Jefferson Girao <contato@jefferson.eti.br>
2010-10-14 17:59:28 -0300

    Playlist Console - Put animated gif on save button - Story ID #4825026

--- public/javascripts/playlist_create.js
+++ public/javascripts/playlist_create.js
@@ -759,6 +759,7 @@ function open_save_popup() {
             update_playlist_avatar('#update_layer_avatar', $('#' + _pv.item_ids[0]).find('img').attr('src').replace(/image\/thumbnail/i, "image/hi-thumbnail"));
           }
           $.popup({ div: '#edit_conf_popup' });
+          $('#save_button span span .btn_red_loading').remove();
         },
         error: function(r){alert('Error!')}
       });
@@ -771,11 +772,6 @@ function open_save_popup() {
   } else {
     $.popup({ div: '#unable_popup' });
   }
-
-  // same name as the save button on the popup itself
-  // $('.btn_red_loading').remove()
-  // remove the loading animation on the save button on the playlist page, not the popup
-  $('#save_button span span .btn_red_loading').remove();
 }
 
 function submit_save_form() {
