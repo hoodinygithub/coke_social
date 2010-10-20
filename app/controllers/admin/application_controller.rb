57a912d079de0caa3bfe14028b8940327bb21884
Jefferson Girao <contato@jefferson.eti.br>
2010-10-20 16:07:40 -0300

    Tags: Admin console - pwd protect - Story ID #5770323

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,14 +2,15 @@ class Admin::ApplicationController < ApplicationController
   layout "admin"
   before_filter :admin_only_in_english
   skip_before_filter :login_required
+  before_filter :do_basic_http_authentication
 
   private
   def do_basic_http_authentication
     authenticate_or_request_with_http_basic do |username, password|
-      username == "hoodiny" && password == "3057227000"
+      username == "happiness" && password == "d0ral8725"
     end
   end
-  
+    
   def admin_only_in_english
     I18n.locale = :en
   end
