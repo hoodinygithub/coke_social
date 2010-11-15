6f85dcc1d9ee6a095b104c515ce2c1bd3ab3aada
Ed Toro <etoro@hoodiny.com>
2010-11-15 16:29:44 -0500

    Perf enhancements for post-comment xmas badge callbacks.

--- app/models/badges/awards.rb
+++ app/models/badges/awards.rb
@@ -70,37 +70,42 @@ module Badges::Awards
 
   def award_xmas_badges_comment
 
+   # cache these for perf
+   user_wins = BadgeAward.all(:conditions => { :winner_id => user.id }).map(&:badge_id)
+   owner_wins = BadgeAward.all(:conditions => { :winner_id => commentable.owner.id }).map(&:badge_id)
+   is_promo_playlist = commentable.promo_playlist?
+
     # Getting promo_tags for every playlist you've commented on may be too heavy.  Try to short circuit if already won.
-    unless Badge.find_by_badge_key("xmas_xpert").winners.include? user
-      if commentable.promo_playlist? and user.comments.all(:include=>:commentable).select{|c| c.commentable.promo_playlist?}.length >= 10
+    unless user_wins.include? Badge.find_by_badge_key("xmas_xpert").id
+      if is_promo_playlist and user.comments.all(:include=>:commentable).select{|c| c.commentable.promo_playlist?}.length >= 10
         award_badge(:xmas_xpert, user)
       end
     end
 
     # award_badge doesn't check for multiple badges per user for each playlist
-    unless Badge.find_by_badge_key("xmas_rocker").winners.with_playlist(commentable.id).include? user
+    unless owner_wins.include? Badge.find_by_badge_key("xmas_rocker").id
       # Assumes each comment must be from a unique user.  No duplicate comments per user.
-      if commentable.promo_playlist? and commentable.comments.count >= 20
+      if is_promo_playlist and commentable.comments.count >= 20
         award_badge_for_playlist(:xmas_rocker, commentable.owner, commentable.id, false)
       end
     end
 
-    if rating == 5 and commentable.promo_playlist?
+    if rating == 5 and is_promo_playlist
       award_badge_for_playlist(:cool_lo, commentable.owner, commentable.id)
     end
 
-    if rating == 5 and commentable.promo_playlist? and commentable.comments.count(:conditions => {:rating => 5}) >= 3
+    if rating == 5 and is_promo_playlist and commentable.comments.count(:conditions => {:rating => 5}) >= 3
       award_badge_for_playlist(:que_bolas, commentable.owner, commentable.id)
     end
 
-    unless Badge.find_by_badge_key("recalentado").winners.include? user
+    unless owner_wins.include? Badge.find_by_badge_key("recalentado").id
       # This line of code is flippin' awesome!  (and heavy)
       if commentable.owner.playlists.all(:include => :comments).select{|p| p.promo_playlist?}.map(&:comments).flatten.map(&:rating).inject({}) { |hash, x| hash[x].nil? ? hash[x] = 1 : hash[x] += 1; hash }.select{|k,v| v >= 2; }.size > 0
         award_badge(:recalentado, commentable.owner) 
       end
     end
 
-    unless Badge.find_by_badge_key("regalo").winners.include? user
+    unless user_wins.include? Badge.find_by_badge_key("regalo").id
       if rating > 3 and user.comments.all(:order => "created_at DESC", :include => :commentable).select{|c| c.commentable.promo_playlist?}[0..4].select{|c| c.rating > 3}.size >= 3 
         award_badge(:regalo, user)
       end
