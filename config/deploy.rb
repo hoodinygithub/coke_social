f3a93f894b3f2e6c5829a997e0b3fd6b9621d823
David Salazar <dsalazar@hoodiny.com>
2010-12-07 12:29:56 -0500

    Changed deploy variable

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,7 @@ ssh_options[:paranoid] = false
 # can also specify options that can be used to single out a specific subset of boxes in a
 # particular role, like :primary => true.
 
-set :branch, "master"
+set :branch, "dl_landing"
 
 # Production
 task :production do
