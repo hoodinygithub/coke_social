d2af9de181330b055024e7169aaa57d80007eebe
Ed Toro <etoro@hoodiny.com>
2011-02-24 16:00:39 -0500

    WindowsConnect v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -49,12 +49,21 @@ class SessionsController < ApplicationController
       render :nothing => true
     elsif !params[:wrap_verification_code].nil?
       # Windows Connect through popup login
-      user = WindowsConnect.parse_verification_code(params[:wrap_verification_code], current_site_url + new_session_path)
+      user = WindowsConnect.parse_verification_code(params[:wrap_verification_code], current_site_url + new_session_path, cookies, params[:wrap_client_state], params[:exp])
+      
+      handle_windows_sso_user(user)
 
       # Close the Windows Connect login popup.  Facebook does this for us.
       respond_to do |format|
         format.html { render 'shared/_close_popup.html.erb', :layout => false }
       end
+    elsif cookies[:wl_internalState]
+      # Clear the Windows Connect session on login page load
+      # This might cause trouble in the future if we decide to use WindowsConnect widgets on the page.
+      # Right now, this is the only way to cancel a bad, currently logged-in WC session.
+      #Rails.logger.info cookies
+      #Rails.logger.info session
+      WindowsConnect.clear_cookie_session(cookies)
     else
       # Cyloop Login
       #render :new, :layout => false
@@ -87,20 +96,23 @@ class SessionsController < ApplicationController
   end
 
 private
-  # This is to get around a domain issue for WLID
+  #
+  # Gets around a domain issue for WLID
+  #
   def corrected_registration_host
     session[:registered_from] || request.host
   end
 
+  #
+  # Login
+  #
   def do_login(account, remember_me, save_wlid=false)
     if account.nil?
       flash[:error] = t("registration.login_failed")
       redirect_to login_path
-      # render :new
       return false
     elsif account.kind_of?(Artist)
       flash[:error] = t("registration.artist_login_denied")
-      #redirect_to(new_user_path)
       render :new
       return false;
     elsif account.part_of_network?
@@ -120,8 +132,13 @@ private
       #end
       
       # Attach current FB session to upcoming user session.
-      sso_id = (session[:sso_user] and session[:sso_user]['sso_facebook']) ? session[:sso_user]['sso_facebook'] : nil
-      account.update_attribute(:sso_facebook, sso_id) if sso_id
+      sso_facebook_id = (session[:sso_user] and session[:sso_user]['sso_facebook']) ? session[:sso_user]['sso_facebook'] : nil
+      account.update_attribute(:sso_facebook, sso_facebook_id) if sso_facebook_id
+      
+      # Attach current WindowsConnect session to upcoming user session.
+      sso_windows_id = (session[:sso_user] and session[:sso_user]['sso_windows']) ? session[:sso_user]['sso_windows'] : nil
+      account.update_attribute(:sso_windows, sso_windows_id) if sso_windows_id
+      
       session[:sso_user] = nil
       session[:sso_type] = nil
       
@@ -139,15 +156,20 @@ private
     end
   end
 
+  # 
+  # Facebook SSO login
+  #
   def handle_facebook_sso_user(p_user)
     return nil if p_user.nil?
 
+    # TODO: account unification search
     same_sso_user = User.find_by_sso_facebook_and_deleted_at p_user.sso_facebook, nil
     unless same_sso_user.nil?
       do_login(same_sso_user, nil)
       return
     end
 
+    # TODO: account unification search
     same_email_user = User.find_by_email(p_user.email, :select => "slug, encrypted_gender, encrypted_email, encrypted_name, encrypted_born_on_string", :conditions => { :deleted_at => nil })
     unless same_email_user.nil?
       # logger.info "Found same email user."
@@ -162,6 +184,38 @@ private
     session[:sso_user] = p_user
     session[:sso_type] = "Facebook"
 
+    redirect_to_page_param
+  end
+  
+  # 
+  # Windows SSO login
+  #
+  def handle_windows_sso_user(p_user)
+    return nil if p_user.nil?
+
+    # AccountUni search
+    ## db enforces uniqueness of sso_windows, deleted_at, so pick first
+    same_sso_user = User.find_with_exclusive_scope(:first, :conditions => { :sso_windows => p_user.sso_windows, :deleted_at => nil })
+    unless same_sso_user.nil?
+      do_login(same_sso_user, nil)
+      return
+    end
+
+    # AccountUni search
+    ## db doesn't enforce uniqueness of msn_live_id, deleted_at, so just pick the most recent one
+    same_wlid_user = User.find_with_exclusive_scope(:first, :conditions => { :msn_live_id => p_user.msn_live_id, :deleted_at => nil }, :order => "created_at DESC")
+    unless same_wlid_user.nil?
+      # Upgrade LiveID user to ConnectID user
+      same_wlid_user.update_attribute(:sso_windows, p_user.sso_windows)
+      do_login(same_wlid_user, nil)
+      return
+    end
+
+    session[:sso_user] = p_user
+    session[:sso_type] = 'Windows'
+  end
+
+  def redirect_to_page_param
     # If user was on reg page, redirect to pre-filled reg page.
     # If user was on login page, redirect to link page.
     if params[:page]
@@ -174,14 +228,7 @@ private
       redirect_to new_user_path
     end
   end
-  
-  def handle_windows_sso_user(p_user)
-    return nil if p_user.nil?
 
-    p_user
-  end
-
-  protected
   def compute_layout
     [:new, :create].include?(action_name.to_sym) ? "no_search_form" : "application" 
   end
--- app/services/windows_connect.rb
+++ app/services/windows_connect.rb
@@ -10,26 +10,121 @@ class WindowsConnect
   TOKEN_PARAMS = { :wrap_client_id => CLIENT_ID, :wrap_client_secret => SECRET }
   WLID_TOKEN_PARAMS = TOKEN_PARAMS.merge :idtype => 'PWID'
 
-  # http[s]://[LocatorID.]apis.live.net/V<Version>/cid-<CID>/<ResourceName>/<CollectionName>/<ID>
-  # ex. http://sn1.apis.live.net/V4.1/cid-C3092E04172BA9C3/Contacts/AllContacts
   API_PROFILES_URL = "https://apis.live.net/V4.1/cid-%s/Profiles"
   API_PROFILES_URL_HEADERS = {"Accept" => "application/json"}
 
   #
   # Parse Windows Connect verification into a User
   #
-  def self.parse_verification_code(p_wrap_verification_code, p_callback)
+  def self.parse_verification_code(p_wrap_verification_code, p_callback, p_cookies, p_wrap_client_state, p_exp)
+    access_token_params = get_access_token(p_wrap_verification_code, p_callback, nil)
+
+    if access_token_params.nil?
+      clear_cookie_session(p_cookies)
+      return nil
+    end
+
+    access_token = access_token_params[:access_token]
+    cid = access_token_params[:uid]
+    expires = access_token_params[:expires]
+
+    # M$ docs are unclear about cookies
+
+    # http://msdn.microsoft.com/en-us/library/gg251990.aspx
+    p_cookies["c_clientId"] = CLIENT_ID
+    p_cookies["c_clientState"] = p_wrap_client_state
+    p_cookies["c_scope"] = p_exp
+    p_cookies["c_accessToken"] = access_token
+    p_cookies["c_expiry"] = expires
+    p_cookies["c_uid"] = cid
+    p_cookies["lca"] = "done"
+
+    # http://msdn.microsoft.com/en-us/library/ff752581.aspx
+    p_cookies["wl_clientID"] = CLIENT_ID
+    p_cookies["wl_clientState"] = p_wrap_client_state
+    p_cookies["wl_scope"] = p_exp
+    p_cookies["wl_accessToken"] = access_token
+    p_cookies["wl_accessTokenExpireTime"] = expires
+    p_cookies["wl_cid"] = cid
+    p_cookies["wl_complete"] = "done"
+
+    user = get_profile_user(cid, access_token)
+
+    populate_pwid(user, p_wrap_verification_code, p_callback, p_cookies)
+  end
+
+  def self.clear_cookie_session(p_cookies)
+    # http://msdn.microsoft.com/en-us/library/gg251990.aspx
+    p_cookies.delete "c_clientId"
+    p_cookies.delete "c_clientState"
+    p_cookies.delete "c_scope"
+    p_cookies.delete "c_accessToken"
+    p_cookies.delete "c_expiry"
+    p_cookies.delete "c_uid"
+    p_cookies.delete "lca"
+
+    # http://msdn.microsoft.com/en-us/library/ff752581.aspx
+    p_cookies.delete "wl_clientID"
+    p_cookies.delete "wl_clientState"
+    p_cookies.delete "wl_scope"
+    p_cookies.delete "wl_accessToken"
+    p_cookies.delete "wl_accessTokenExpireTime"
+    p_cookies.delete "wl_cid"
+    p_cookies.delete "wl_complete"
+
+    # Undocumented, but found in the wild.
+    p_cookies.delete "wl_internalState"
+  end
+
+  private
+
+  # 
+  # GET request
+  #
+  def self.remote_get(p_url, p_headers)
+    Rails.logger.info "GET " + p_url
+    Rails.logger.info p_headers.inspect
+    response = 
+      begin
+        RestClient.get p_url, p_headers
+      rescue => e
+        Rails.logger.error e.to_s
+        nil
+      end
+    Rails.logger.info response.to_s
+    response
+  end
+
+  #
+  # POST request
+  #
+  def self.remote_post(p_url, p_params)
+    Rails.logger.info "POST " + p_url
+    Rails.logger.info p_params.inspect
+    response = RestClient.post p_url, p_params
+    Rails.logger.info response.to_s
+    response
+  end  
+
+  #
+  # Get the access token, user id, and other stuff.
+  #
+  def self.get_access_token(p_wrap_verification_code, p_callback, p_idtype) 
     params = TOKEN_PARAMS.merge :wrap_verification_code => p_wrap_verification_code, :wrap_callback => p_callback
-    response = get_access_token(TOKEN_URL, params)
+    # default idtype=CID
+    params.merge! :idtype => p_idtype if p_idtype
+    response = remote_post(TOKEN_URL, params)
+
+    return nil if response.nil?
 
     if response.code == 200
       response_params = CGI::parse(response.to_s)
 
       access_token = response_params['wrap_access_token'][0].to_s
       cid = response_params['uid'][0].to_s
-      #expires = response_params['wrap_access_token_expires_in'][0].to_s
-      #wrap_refresh_token = response_params['wrap_refresh_token']
-      #skey = response_params['skey']
+      expires = response_params['wrap_access_token_expires_in'][0].to_s
+      wrap_refresh_token = response_params['wrap_refresh_token']
+      skey = response_params['skey']
 
       Rails.logger.info 'access_token: ' + access_token
       Rails.logger.info 'cid: ' + cid
@@ -37,76 +132,101 @@ class WindowsConnect
       #Rails.logger.info 'refresh token: ' + wrap_refresh_token
       #Rails.logger.info 'secret key: ' + skey
 
-      # Get profile data
-      auth_headers = API_PROFILES_URL_HEADERS.merge "Authorization" => access_token
-      response = get_profile((API_PROFILES_URL % cid.to_s), auth_headers)
-      # Supports multiple profiles per user.  Don't know what that means, so select the 1st one.
-      response_profile = JSON::parse(response)['Entries'][0]
-      
-      # Gender: 0 = male, 1 = female (I'm guessing.)
-      gender = (response_profile['Gender'] == 0 ? 'Male' : 'Female')
-      Rails.logger.info 'gender: ' + gender
-      # Supports multiple email "Types".  Don't know what that means, so select the 1st one.
-      email = response_profile['Emails'][0]['Address']
-      Rails.logger.info 'email: ' + email
-
-      # Where the f is the name?      
-      user = User.new(
-      	:email => email,
-      	:gender => gender,
-      	:slug => email.split("@")[0],
-      	:sso_windows => cid
-      )
-      Rails.logger.info user.inspect
-
-      cid_user = User.find_by_sso_windows_and_deleted_at(cid, nil)
-      if cid_user
-        # User found by CID.
-        return cid_user
-      else
-        # Not found with CID.  Try with old PWID.
-        params.merge! :idtype => 'PWID'
-        response = get_access_token(TOKEN_URL, params)
-
-        if response.code == 200
-          response_params = CGI::parse(response.to_s)
-          access_token = response_params['wrap_access_token'][0]
-          wlid = response_params['uid'][0]
-          expires = response_params['wrap_access_token_expires_in'][0]
-
-          return User.find_by_msn_live_id_and_deleted_at(wlid, nil)
-        else
-          Rails.logger.error response.inspect
-          return nil
-        end
-      end
+      { :access_token => access_token, :uid => cid, :expires => expires, :wrap_refresh_token => wrap_refresh_token, :skey => skey }
     else
       Rails.logger.error response.inspect
-      return nil
+      nil
     end
   end
 
-  private
+  #
+  # Get and parse the profile from the REST JSON API
+  #
+  def self.get_profile_user(p_cid, p_access_token)
+    auth_headers = API_PROFILES_URL_HEADERS.merge "Authorization" => p_access_token
+    response = remote_get((API_PROFILES_URL % p_cid.to_s), auth_headers)
+
+    return nil if response.nil?
+
+    # Supports multiple profiles per user.  Select the 1st one.
+    response_profile = JSON::parse(response)['Entries'][0]
+
+    # Gender: 0 = undefined, 1 = female, 2 = male
+    # http://msdn.microsoft.com/en-us/library/ff747447.aspx
+    gender = case response_profile['Gender']
+             when 1 then 'Female'
+             when 2 then 'Male'
+             else nil
+             end
+    Rails.logger.info "gender: #{gender}"
+    # Supports multiple email "Types".  0 = none, 1 = home, 2 = work, 3 = ?.  Just picking the 1st one I find.
+    email = response_profile['Emails'] ? response_profile['Emails'][0]['Address'] : nil
+    Rails.logger.info "email: #{email}"
+
+    name = response_profile['FirstName'].to_s + ' ' + response_profile['LastName'].to_s
+    Rails.logger.info "name: #{name}"
+
+    # BirthDay and BirthYear are not available.  Only BirthMonth.
+    # http://msdn.microsoft.com/en-us/windowslive/gg465294.aspx
+    birthYear = response_profile['BirthYear'].nil? ? Date.today.year : response_profile['BirthYear']
+    birthMonth = response_profile['BirthMonth'].nil? ? Date.today.month : response_profile['BirthMonth']
+    birthDay = response_profile['BirthDay'].nil? ? Date.today.day : response_profile['BirthDay']
+    dob = Date.new(birthYear, birthMonth, birthDay)
+    Rails.logger.info "dob: #{dob}"
+
+    # Sometimes I get an empty Profile object.
+    return nil if email.nil?
+
+    user = User.new(
+      :name => name,
+      :email => email,
+      :born_on => dob,
+      :gender => gender,
+      :slug => email.split("@")[0],
+      :sso_windows => p_cid
+    )
+    Rails.logger.info user.inspect
+
+    user
+  end
 
   #
-  # POST request for the access token and user id
+  # Populate the user.msn_live_id, if necessary.
+  # Necessary if the lookup by sso_windows would fail and we'll have to search again by msn_live_id afterwards.
   #
-  def self.get_access_token(p_url, p_params) 
-    Rails.logger.info "POST " + p_url
-    Rails.logger.info p_params.inspect
-    response = RestClient.post p_url, p_params
-    Rails.logger.info response.to_s
-    response
+  def self.populate_pwid(p_user, p_wrap_verification_code, p_callback, p_cookies)
+    # This check is redundant.  It happens again later when we handle the user.
+    # The query results should be cached for the 2nd lookup.
+    #
+    # We peek ahead now to determine whether we need to make another trip to the API to get the PWID.
+    # As more users transition to CID, the 2nd API call becomes less frequent, and this method becomes faster.
+    cid_user = User.find_with_exclusive_scope(:first, :conditions=>{:sso_windows => p_user.sso_windows, :deleted_at => nil})
+    if cid_user
+      # User found by CID.
+      return cid_user
+    else
+      # Need the old PWID
+      access_token_params = get_access_token(p_wrap_verification_code, p_callback, 'PWID')
+
+      access_token = access_token_params[:access_token]
+      pwid = access_token_params[:uid]
+      expires = access_token_params[:expires]
+
+      # http://msdn.microsoft.com/en-us/library/gg251990.aspx
+      p_cookies["c_accessToken"] = access_token
+      p_cookies["c_expiry"] = expires
+
+      # http://msdn.microsoft.com/en-us/library/ff752581.aspx
+      p_cookies["wl_accessToken"] = access_token
+      p_cookies["wl_accessTokenExpireTime"] = expires
+
+      Rails.logger.info access_token
+      Rails.logger.info pwid
+      Rails.logger.info expires
+
+      p_user.msn_live_id = pwid
+      p_user
+    end
   end
-  
-  # 
-  # GET request for profile data
-  #
-  def self.get_profile(p_url, p_headers)
-    Rails.logger.info "GET " + p_url
-    Rails.logger.info p_headers.inspect
-    response = RestClient.get p_url, p_headers
-    Rails.logger.info response.to_s
-    response
-  end
+
 end
