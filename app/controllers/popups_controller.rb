6231b400902b81bfd8f26a7ef5c90476c6dd03c7
David Salazar <dsalazar@hoodiny.com>
2011-03-07 15:24:12 -0500

    Popup mix player, background/page upd
    Squashed commit of the following:
    
    commit 10196dd0b237073e8978ea527aa71c50cbcd7f22
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Mon Mar 7 15:23:34 2011 -0500
    
        Removed cultural relevance background
    
    commit edba9dfc991ccfc04e6c8d6c7cf4732731bd185e
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Fri Mar 4 17:29:05 2011 -0500
    
        Updated images for mix button
    
    commit fc560661c171bd6c784a3c2541365ca27bad40f1
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Fri Mar 4 11:00:33 2011 -0500
    
        css tweak for ssl pages
    
    commit c04e9658b62583f15231d6b7042ba38c0d17dbe1
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Fri Mar 4 10:46:41 2011 -0500
    
        Brought back cyloop logo
    
    commit 3fd5ffc7714067eaab8c840bc05cae80014e0a77
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Fri Mar 4 10:33:56 2011 -0500
    
        Removed cyloop powered logo
    
    commit 7dff4f761bc0505ba705e858c902f10344b6e39f
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Fri Mar 4 10:31:01 2011 -0500
    
        Changed target urls on social buttons and removed layer on create btn
    
    commit bcf61a5268dd15083848d706dc958772d2a44fd3
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Thu Mar 3 17:39:51 2011 -0500
    
        Tweaked css for fb like button
    
    commit d8b237358d9abb5cfd715525777673bf7e6b8103
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Thu Mar 3 17:25:04 2011 -0500
    
        Switched order on social components
    
    commit b4a160352bbf1752bb1945748bc09e85aab1a5eb
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Thu Mar 3 16:07:45 2011 -0500
    
        Updated bg
    
    commit 8d121b140d401d247412d02a62602bc26e6e15c4
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Thu Mar 3 15:32:50 2011 -0500
    
        Updated badges
    
    commit 0b414f371bdc22bbdefca9684941d80d6e878465
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Thu Mar 3 15:00:10 2011 -0500
    
        max plays layer
    
    commit 84a26649b897b7d551f0e86f639010a34aaf3086
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Thu Mar 3 14:52:36 2011 -0500
    
        Removed border top
    
    commit d03f46e0d0d75d555eeb73ac487b46c81b5f2547
    Author: David Salazar <dsalazar@hoodiny.com>
    Date:   Thu Mar 3 14:36:51 2011 -0500
    
        Coke popup mix player

--- app/controllers/popups_controller.rb
+++ app/controllers/popups_controller.rb
@@ -1,5 +1,13 @@
 class PopupsController < ApplicationController
-  layout 'widget'
+
+  layout 'popup'
+
+  def index
+    @station_obj = nil
+    station = Station.find_by_id_and_playable_type(params[:station_id], 'Playlist') rescue nil
+    @station_obj = station if station and station.playable and station.playable.owner and station.playable.owner.active?
+    @station_queue = @station_obj.playable.station_queue(:ip_address => remote_ip) if @station_obj
+  end
   
   def widget
     respond_to do |format|
