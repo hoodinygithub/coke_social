a4f59a695bc486b41de14a0b2efa4577095a57b2
Ed Toro <etoro@hoodiny.com>
2011-04-20 01:37:54 -0400

    perf: index playlists.last_played_at for order_by

--- db/migrate/20110418165043_last_played_at_to_playlist.rb
+++ db/migrate/20110418165043_last_played_at_to_playlist.rb
@@ -1,9 +1,11 @@
 class LastPlayedAtToPlaylist < ActiveRecord::Migration
   def self.up
     add_column :playlists, :last_played_at, :timestamp
+    add_index :playlists, [:site_id, :last_played_at]
   end
 
   def self.down
+    remove_index :playlists, [:site_id, :last_played_at]
     remove_column :playlists, :last_played_at
   end
 end
