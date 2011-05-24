4d9f97c3cb347b6954fd27ddbc51dae9882406b8
Ed Toro <etoro@hoodiny.com>
2011-05-24 18:28:50 -0400

    Reg link should go to SSL page.  In the future it should load in a secure IFRAME.

--- app/helpers/registration_layers_helper.rb
+++ app/helpers/registration_layers_helper.rb
@@ -12,13 +12,13 @@ module RegistrationLayersHelper
     "#{current_site.code.to_s}/1.jpg"
   end
 
-  def registered_link_to(text, params)
-    options = {}
-    [:class, :title].each do |i|
-      options[i] = params.delete(i)
-    end
-    
-    link_to text, new_user_path(params), options
+  def registered_link_to(text, params=nil, options=nil)
+    url = if params.has_key? :host
+            new_user_url(params)
+          else
+            new_user_path(params)
+          end
+    link_to text, url, options
   end
 
 end
