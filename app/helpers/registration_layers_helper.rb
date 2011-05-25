abcdb0cb585aa7f9481353f971480bf4aa031856
Ed Toro <etoro@hoodiny.com>
2011-05-25 19:35:07 -0400

    Reg link automatically redirects to SSL path, so no need to specify it directly.  Old helper was for the old Windows Live login, which we don't use anymore.

--- app/helpers/registration_layers_helper.rb
+++ app/helpers/registration_layers_helper.rb
@@ -12,13 +12,4 @@ module RegistrationLayersHelper
     "#{current_site.code.to_s}/1.jpg"
   end
 
-  def registered_link_to(text, params=nil, options=nil)
-    url = if params.has_key? :host
-            new_user_url(params)
-          else
-            new_user_path(params)
-          end
-    link_to text, url, options
-  end
-
 end
