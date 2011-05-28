31342b50b3a3cc523de20ab743b2c5dd70b9a1b5
Ed Toro <etoro@hoodiny.com>
2011-05-28 13:56:45 -0400

    Add unique index to prevent multiple station rows for a single playable.

---
+++ db/migrate/20110528175000_add_unique_index_to_station_playables.rb
@@ -0,0 +1,11 @@
+class AddUniqueIndexToStationPlayables < ActiveRecord::Migration
+  def self.up
+    remove_index :stations, [:playable_type, :playable_id]
+    add_index :stations, [:playable_type, :playable_id], :unique => true
+  end
+
+  def self.down
+    remove_index :stations, [:playable_type, :playable_id], :unique => true
+    add_index :stations, [:playable_type, :playable_id]
+  end
+end
