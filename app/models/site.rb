cd81fcb7cc54094071c6d8923bb8c2e979cc1892
Ed Toro <etoro@hoodiny.com>
2011-05-31 19:35:59 -0400

    pref: includes cache country with top djs

--- app/models/site.rb
+++ app/models/site.rb
@@ -45,7 +45,7 @@ class Site < ActiveRecord::Base
   has_many :top_user_stations, :through => :summary_top_user_stations, :class_name => 'UserStation', :foreign_key => 'user_station_id', :source => :user_station, :order => 'top_user_stations.total_requests DESC'
 
   has_many :summary_top_djs, :order => 'total_requests DESC', :class_name => 'TopDj', :include => :dj
-  has_many :top_djs, :through => :summary_top_djs, :class_name => 'User', :foreign_key => 'dj_id', :source => :user, :order => 'top_djs.total_requests DESC', :conditions => 'networks.id = 2 AND accounts.deleted_at IS NULL', :include => :networks
+  has_many :top_djs, :through => :summary_top_djs, :class_name => 'User', :foreign_key => 'dj_id', :source => :user, :order => 'top_djs.total_requests DESC', :conditions => 'networks.id = 2 AND accounts.deleted_at IS NULL', :include => [:networks, :country]
 
   has_many :summary_top_playlists, :order => 'total_requests DESC', :class_name => 'TopPlaylist', :include => :playlist
   has_many :top_playlists, :through => :summary_top_playlists, :class_name => 'Playlist', :foreign_key => 'playlist_id', :source => :playlist, :order => 'top_playlists.total_requests DESC', :include => [:owner, :station], :conditions => 'playlists.deleted_at IS NULL and accounts.deleted_at IS NULL'
