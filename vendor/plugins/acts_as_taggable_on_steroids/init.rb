54c19d6c409de6e7926c5667da1bcf1bc4dcc17c
Ed Toro <etoro@hoodiny.com>
2011-06-01 15:01:51 -0400

    cleanup Tag monkeypatch that was moved to config/initializers/

--- vendor/plugins/acts_as_taggable_on_steroids/init.rb
+++ vendor/plugins/acts_as_taggable_on_steroids/init.rb
@@ -1,18 +1,2 @@
 require File.dirname(__FILE__) + '/lib/acts_as_taggable'
 
-Tag.class_eval do
-
-  has_many :valid_tags #, :conditions => { :site_id => ApplicationController.current_site.id }
-  # ApplicationController.current_site isn't defined yet
-
-  def nickname
-    #ValidTag.find_by_tag_id(self.id).try(:promo_id).blank? ? name : name.try(:upcase).try(:gsub, 'ñ', 'Ñ').try(:gsub, 'á', 'Á')
-    # use select instead of find conditions to take advantage of include caching
-    valid_tags.select{|t| t.site_id == ApplicationController.current_site.id}.first.promo_id.blank? ? name : name.upcase.gsub('ñ', 'Ñ').gsub('á', 'Á')
-  end
-  
-  def to_s
-    nickname
-  end
-  
-end
