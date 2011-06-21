d00a02c8ede5d9c3d7f079b20281a27586d9e1ea
Ed Toro <etoro@hoodiny.com>
2011-06-21 18:05:05 -0400

    Remove create_index from migration

--- db/migrate/20110217181034_add_sso_windows_id_to_account.rb
+++ db/migrate/20110217181034_add_sso_windows_id_to_account.rb
@@ -1,11 +1,11 @@
 class AddSsoWindowsIdToAccount < ActiveRecord::Migration
   def self.up
     add_column :accounts, :sso_windows, :string
-    add_index :accounts, [:sso_windows, :deleted_at], :unique => true
+    #add_index :accounts, [:sso_windows, :deleted_at], :unique => true
   end
 
   def self.down
-    remove_index :accounts, [:sso_windows, :deleted_at]
+    #remove_index :accounts, [:sso_windows, :deleted_at]
     remove_column :accounts, :sso_windows
   end
 end
