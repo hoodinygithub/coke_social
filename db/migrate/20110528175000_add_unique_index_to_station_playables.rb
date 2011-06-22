5e4715c0a9c3998d5fa03f278916b77a2e52703d
Ed Toro <etoro@hoodiny.com>
2011-06-21 22:47:18 -0400

    disable some more migrations for later manual

--- db/migrate/20110528175000_add_unique_index_to_station_playables.rb
+++ db/migrate/20110528175000_add_unique_index_to_station_playables.rb
@@ -1,7 +1,7 @@
 class AddUniqueIndexToStationPlayables < ActiveRecord::Migration
   def self.up
-    remove_index :stations, [:playable_type, :playable_id]
-    add_index :stations, [:playable_type, :playable_id], :unique => true
+    #remove_index :stations, [:playable_type, :playable_id]
+    #add_index :stations, [:playable_type, :playable_id], :unique => true
   end
 
   def self.down
