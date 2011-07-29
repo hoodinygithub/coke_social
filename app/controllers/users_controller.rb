939cce3d2180859ae3f28be8cb5ed165e05e7c73
David Salazar <dsalazar@prisadigital.com>
2011-07-29 17:22:03 -0400

    Removed double render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
       Resque.enqueue(TwitterJob, {:user_id => @user.id, :twitter_username => @user.twitter_username}) if twitter_username_changed
       flash[:success] = t('settings.saved')
       #return redirect_to(edit_my_settings_path)
-      render :action => :edit
+      #render :action => :edit
     else
       flash[:error] = t('settings.not_saved')
     end
