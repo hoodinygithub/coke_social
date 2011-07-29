8cec4c8b571465740c17f57166627282e5b09dc9
David Salazar <dsalazar@prisadigital.com>
2011-07-29 16:46:43 -0400

    Added type put

--- public/javascripts/coke/base.js
+++ public/javascripts/coke/base.js
@@ -435,6 +435,7 @@ var restoreInput = function(value, input) {
 Base.account_settings.edit = function() {
   var form = $(event.srcElement).closest('form');
   form.ajaxSubmit({
+    type: "POST",
     success: function(response)
     {
       Base.UI.contentswp(response);
