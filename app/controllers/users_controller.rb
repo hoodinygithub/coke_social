0e68717609fa546f0e6c85f9ebe425a3b3b28885
Ed Toro <etoro@prisadigital.com>
2011-07-29 20:29:32 -0400

    fix account settings validation errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,13 +60,10 @@ class UsersController < ApplicationController
     if @user.save
       Resque.enqueue(TwitterJob, {:user_id => @user.id, :twitter_username => @user.twitter_username}) if twitter_username_changed
       flash[:success] = t('settings.saved')
-      return redirect_to(edit_my_settings_path(:ajax => 1))
-      #render :action => :edit
     else
       flash[:error] = t('settings.not_saved')
     end
-    render :action => :edit
-
+    redirect_to(edit_my_settings_path(:ajax => 1))
   end
 
   # GET /users/new
